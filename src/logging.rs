e26c6eb5c005c0d8840a5bfcef0d4ce6b3968dfd
recate-rat237 <92317688+recate-rat237@users.noreply.github.com>
2023-04-13 19:31:36 +0300

    Update logging.rs

--- src/logging.rs
+++ src/logging.rs
@@ -7,26 +7,29 @@ pub enum MsgStructElement {
 
 #[derive(Clone)]
 pub struct RLogger {
-    msg_struct: Vec<MsgStructElement>
+    msg_struct: Vec<MsgStructElement>,
+    log_level: i32
 }
 
 
 impl RLogger {
-    pub fn new(msg_struct: Vec<MsgStructElement>) -> RLogger {
-        RLogger { msg_struct: msg_struct }
+    pub fn new(msg_struct: Vec<MsgStructElement>, log_level: i32) -> RLogger {
+        RLogger { msg_struct: msg_struct, log_level: log_level }
     }
 
-    pub fn log(self, msg: &str, mark: &str) {
-        for i in self.msg_struct {
-            if i == MsgStructElement::Mark {
-                print!("[{}]", mark);
-            } else if i == MsgStructElement::Message {
-                print!("{}", msg);
-            } else if i == MsgStructElement::DateAndTime {
-                print!("[{}]", chrono::Utc::now());
-            } 
+    pub fn log(self, log_level: i32, msg: &str, mark: &str) {
+        if log_level >= self.log_level {
+            for i in self.msg_struct {
+                if i == MsgStructElement::Mark {
+                    print!("[{}]", mark);
+                } else if i == MsgStructElement::Message {
+                    print!("{}", msg);
+                } else if i == MsgStructElement::DateAndTime {
+                    print!("[{}]", chrono::Utc::now());
+                } 
+            }
+            println!();
         }
-        println!();
     }
 
 }
