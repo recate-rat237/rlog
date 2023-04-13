a6e446f8df3fcc84f73ac1550a7720f6f20358b3
recate-rat237 <92317688+recate-rat237@users.noreply.github.com>
2023-04-13 18:37:19 +0300

    x
    x

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,3 @@
-pub mod rlog {
-    
-    #[derive(PartialEq, Clone)]
-    pub enum MsgStructElement {
-        DateAndTime,
-        Message,
-        Mark
-    }
+pub mod rlog;
 
-    #[derive(Clone)]
-    pub struct RLogger {
-        msg_struct: Vec<MsgStructElement>
-    }
-
-
-    impl RLogger {
-        pub fn new(msg_struct: Vec<MsgStructElement>) -> RLogger {
-            RLogger { msg_struct: msg_struct }
-        }
-
-        pub fn log(self, msg: &str, mark: &str) {
-            for i in self.msg_struct {
-                if i == MsgStructElement::Mark {
-                    print!("[{}]", mark);
-                } else if i == MsgStructElement::Message {
-                    print!("{}", msg);
-                } else if i == MsgStructElement::DateAndTime {
-                    print!("[{}]", chrono::Utc::now());
-                } 
-            }
-            println!();
-        }
-
-    }
-}
+pub use rlog::rlog::*;
---
+++ src/rlog.rs
@@ -0,0 +1,35 @@
+pub mod rlog {
+    
+    #[derive(PartialEq, Clone)]
+    pub enum MsgStructElement {
+        DateAndTime,
+        Message,
+        Mark
+    }
+
+    #[derive(Clone)]
+    pub struct RLogger {
+        msg_struct: Vec<MsgStructElement>
+    }
+
+
+    impl RLogger {
+        pub fn new(msg_struct: Vec<MsgStructElement>) -> RLogger {
+            RLogger { msg_struct: msg_struct }
+        }
+
+        pub fn log(self, msg: &str, mark: &str) {
+            for i in self.msg_struct {
+                if i == MsgStructElement::Mark {
+                    print!("[{}]", mark);
+                } else if i == MsgStructElement::Message {
+                    print!("{}", msg);
+                } else if i == MsgStructElement::DateAndTime {
+                    print!("[{}]", chrono::Utc::now());
+                } 
+            }
+            println!();
+        }
+
+    }
+}
